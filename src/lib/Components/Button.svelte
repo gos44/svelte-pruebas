d0d382b2cf864e20f7513aedb9c53e957c458e9d
gos44 <gasc2004@gmail.com>
2025-05-15 10:29:55 -0500

    feat: agregar generación de notificaciones con UUID y contenido aleatorio

--- src/lib/Components/Button.svelte
+++ src/lib/Components/Button.svelte
@@ -1,129 +1,118 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
-	import type { HTMLButtonAttributes } from 'svelte/elements';
-	import type { HTMLAnchorAttributes } from 'svelte/elements';
-	
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
 	let isLeftHovered = $state(false);
+
 	let button: HTMLButtonElement | HTMLAnchorElement;
 
-	type Props =(
-	| (HTMLButtonAttributes & {href?:never})
-	| (HTMLAnchorAttributes & {href:string})) & {
+	type Props = (
+		| (HTMLButtonAttributes & { href?: never })
+		| (HTMLAnchorAttributes & { href: string })
+	) & {
 		left?: Snippet<[boolean]>;
 		right?: Snippet;
 		children: Snippet<[boolean]>;
 		size?: 'sm' | 'lg';
 		shadow?: boolean;
 		onlefthover?: () => void;
-		
-
 	};
-	let { left, right, size = 'sm', shadow = false, children,onlefthover, ...props }: Props = $props();
-	export function focus(){
+	let {
+		left,
+		right,
+		size = 'sm',
+		shadow = false,
+		children,
+		onlefthover,
+		...props
+	}: Props = $props();
+
+	export function focus() {
 		button.focus();
 	}
-	export function getButton(){
+
+	export function getButton() {
 		return button;
 	}
 </script>
 
 <svelte:element
-this={props.href? 'a':'button'}
-bind:this={button}
-class:sm={size == 'sm'}
-class:lg={size == 'lg'}
-class:shadow {...props}
-class="button"
+	this={props.href ? 'a' : 'button'}
+	bind:this={button}
+	class="button"
+	class:sm={size == 'sm'}
+	class:lg={size == 'lg'}
+	class:shadow
+	{...props}
 >
-<div class="flex">
-	{#if left}
-		<div
-			role="presentation"
-			class="left-content"
-			onmouseenter={() => {
-				onlefthover?.();
-				isLeftHovered = true;
-			}}
-			onmouseleave={() => {
-				isLeftHovered = false;
-			}}
-		>
-			{@render left(isLeftHovered)}
-		</div>
-	{/if}
-	{@render children(isLeftHovered)}
-	{#if right}
-		<div class="right-content">
-			{@render right()}
-		</div>
-	{/if}
+	<div class="flex">
+		{#if left}
+			<div
+				role="presentation"
+				class="left-content"
+				onmouseenter={() => {
+					onlefthover?.();
+					isLeftHovered = true;
+				}}
+				onmouseleave={() => {
+					isLeftHovered = false;
+				}}
+			>
+				{@render left(isLeftHovered)}
+			</div>
+		{/if}
+		{@render children(isLeftHovered)}
+		{#if right}
+			<div class="right-content">
+				{@render right()}
+			</div>
+		{/if}
 	</div>
 </svelte:element>
 
 <style lang="scss">
-	 .button {
-        background-color: var(--buttonBgColor,#d8c625);
-        border: none;
-        color:var(--buttonTextColor,#fffffd);
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        font-weight: bold;
-        margin: 4px 2px;
-        cursor: pointer;
+	.button {
+		border: none;
+		background-color: var(--buttonBgColor, #ff3e00);
+		color: var(--buttonTextColor, #ffffff);
+		padding: 0 20px;
+		height: 45px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+		display: inline-block;
 		font-family: sans-serif;
-		.flex{
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-		}
-		background-image: linear-gradient(to right, #a7af3b, #5f37f0);
-		transition: all 0.3s ease-in-out;
-		&:focus {
-			outline: none;
-		}
-		&:active {
-			transform: scale(0.95);
+		text-decoration: none;
+		.flex {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
 		}
 		&:disabled {
-			background-color: #ccc;
-			color: #666;
-			cursor: not-allowed;
-		}
-        border-radius: 4px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        &.sm {
-		height: 45px;
-	}
-	&.lg {
-		height: 45px;
-		font-size: 20px;
-	}
-	&.shadow {
-		box-shadow: 0 0 10px rgba(1, 1, 0, 3);
-	}
-    &:disabled {
 			opacity: 0.6;
 			cursor: not-allowed;
 		}
-	&:hover {
-			background-image: linear-gradient(to right, #eb0e0e, #2600ff);
-			color: #fff;
+		&:hover {
+			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
 		}
-	&:active {
-			background-image: linear-gradient(to right, #a7af3b, #5f37f0);
+		&:active {
+			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
 		}
-	.left-content {
-		margin-inline-end: 10px;
+		&.sm {
+			height: 45px;
+		}
+		&.lg {
+			height: 55px;
+			font-size: 20px;
+		}
+		&.shadow {
+			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
+		}
+		.left-content {
+			margin-inline-end: 10px;
+		}
+		.right-content {
+			margin-inline-start: 10px;
+		}
 	}
-
-	.right-content {
-		margin-inline-start: 10px;
-	}
-}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,21 @@
 <script lang="ts">
-	let array = $state([1, 2, 3, 4]);
-	let objectsArray = $state([{ id: 1 }, { id: 2 }]);
-	let object = $state({
-		firstName: 'gos',
-		lastName: 'andes',
-		address: {
-			city: 'City',
-			street: 'Street'
-		}
-	});
-    $effect(() => {
-        console.log(object);
-    });
-    // $inspect(object).with(console.trace);
-
-    // $effect(()=>{
-    //     $inspect.trace();
-    //     console.log(object.firstName)
-    //     console.log(object.address.city)
-    // })
+	import generateNotifications from '$lib/Utils/generation-notifications';
+	let notifications = $state(generateNotifications());
+    console.log(notifications);     
 </script>
-{@debug object,array}
-<h2>{object.firstName}</h2>
-<h2>{object.address.city}</h2>
 
-<input bind:value={object.firstName} />
-<input bind:value={object.address.city} />
-<input bind:value={object.address.street} />
-<p>{array}</p>
-<button
-	onclick={() => {
-		array[1] = Math.random() * 10;
-	}}>Add to array</button>
-
-<button onclick={() => {
-    console.log($state.snapshot(object));
-}}>
-    log Snapshot
-</button>
+<ul>
+	{#each notifications as { title, body, date }}
+		{@const dateObject = new Date(date)}
+		<li>
+			<h5>{title}</h5>
+			<p>{body}</p>
+			<time datetime={dateObject.toISOString()}>{dateObject.toLocaleDateString()}</time>
+		</li>
+	{:else}
+		<p>No notifications</p>
+	{/each}
+</ul>
 
 <style>
 	:global {
