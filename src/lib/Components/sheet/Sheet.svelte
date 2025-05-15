2587e127034b1b70d7e085927545f6f02862a2dc
gos44 <gasc2004@gmail.com>
2025-05-15 13:50:43 -0500

    feat: agregar componente Sheet y utilidades para manejo de celdas

---
+++ src/lib/Components/sheet/Sheet.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { alphabetToNumber, cellToIndex, numberToAlphabet } from "./sheet-utils";
+
+    console.log(alphabetToNumber("z")); // 1
+    console.log(numberToAlphabet(26)); // 1
+    console.log(cellToIndex("A1")); // 0
+
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
-	import Notification from '$lib/components/Notification.svelte';
-	import generateNotifications from '$lib/Utils/generation-notifications';
-	let notifications = $state.raw(generateNotifications(3));
-	$inspect(notifications);
+	import Sheet from "$lib/Components/sheet/Sheet.svelte";
+
+
 </script>
 
-<button onclick={() => {
-    notifications = generateNotifications(3);
-}}>reset</button>
-
-<ul>
-	{#each notifications as notification, index (notification.id)}
-		<li>
-			<Notification
-				{notification}
-				onremove={(id) => {
-					notifications = notifications.filter((n) => n.id !== id);
-                    $inspect(notifications);
-				}}
-			/>
-		</li>
-	{:else}
-		<p>No hay notifications</p>
-	{/each}
-</ul>
+<Sheet />
 
 <style>
 	:global {
@@ -33,12 +14,4 @@
 		}
 	}
 
-	ul {
-		list-style: none;
-		padding: 10px;
-		margin: 0;
-		li {
-			margin-bottom: 10px;
-		}
-	}
 </style>
