2819f7dffd3b7436434e1c1e5f427fb46b344980
gos44 <gasc2004@gmail.com>
2025-05-19 15:16:21 -0500

    feat: agregar dependencias de canvas y konva, y refactorizar +page.svelte para utilizar konva en lugar de ClickToCount y Counters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
-	import Counters from '$lib/components/Counters.svelte';
-	import ClickToCount from '$lib/components/ClickToCount.svelte';
+	import konva from 'konva';
+	let container: HTMLDivElement;
+	$effect(()=>{
+		const stage = new konva.Stage({
+			container,
+			width: 500, // corregido
+			height: 500
+		})
+		const layer = new konva.Layer(); // corregido
+		const rect1 = new konva.Rect({
+			x: 20,
+			y: 20,
+			width: 100,
+			height: 60,
+			fill: 'purple',
+			stroke: 'white',
+			strokeWidth: 4
+		});
+		layer.add(rect1);
+		stage.add(layer);
+	})
 </script>
 
-<ClickToCount />
-
-<Counters>
-	<ClickToCount />
-	<ClickToCount />
-</Counters>
-
-<Counters>
-	<ClickToCount />
-	<ClickToCount />
-</Counters>
-<ClickToCount />
+<div bind:this={container}></div>
 
 <style>
 	:global {
